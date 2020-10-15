6c8f9ac01bae5984df9dc589189ce06cabe8606f
Aaron Santos <aaronpedro16@gmail.com>
2020-10-15 20:16:42 -0300

    add env variables

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import Menu from 'araceae-ui/build/Menu';
 import { ThemeProvider } from 'araceae-ui';
 import Pages from './pages';
-// import { Container } from './styles';
 
+const {
+  REACT_APP_GRATITUDE_APP_NAME: gratitudeAppName,
+  REACT_APP_GRATITUDE_URL: gratitudeUrl,
+  REACT_APP_CONCEPTS_APP_NAME: conceptsAppName,
+  REACT_APP_CONCEPTS_URL: conceptsUrl,
+} = process.env;
 const src: React.FC = () => {
   return (
     <ThemeProvider
@@ -11,8 +16,16 @@ const src: React.FC = () => {
     >
       <Menu
         itens={[
-          { name: 'Gratidão', href: '/', selected: true },
-          { name: 'Conceitos', href: '/conhecimento', selected: false },
+          {
+            name: gratitudeAppName!,
+            href: gratitudeUrl!,
+            selected: true,
+          },
+          {
+            name: conceptsAppName!,
+            href: conceptsUrl!,
+            selected: false,
+          },
         ]}
       />
       <Pages />
